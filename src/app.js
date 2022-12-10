48c31e3b9c289483fab1f2201c8430ac20c31628
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-10 18:41:16 -0500

    completed app.js file in src folder

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BucketList from './components/BucketList';
+
+function App() {
+  return (
+
+    <div className="bucket-app">
+      <BucketList />
+    </div>
+    
+  );
+}
+
+export default App;
