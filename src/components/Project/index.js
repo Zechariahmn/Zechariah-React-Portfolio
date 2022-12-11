e85260f92252b1462e34bae06fe222718b0e5e70
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-10 20:49:08 -0500

    added project index js file layout part 1

---
+++ src/components/Project/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+import  from '../../assets/images/';
+import  from '../../assets/images/';
+import  from '../../assets/images/';
+import  from '../../assets/images/';
+import  from '../../assets/images/';
+import  from '../../assets/images/';
+import  from '../../assets/images/';
+
+function Project() {
+    const applications = [
+        {
+            title: '',
+            appLink: '',
+            repoLink: ' ',
+            languages: '',
+            picture: 
+        },
+        {
+            title: '',
+            appLink: '',
+            repoLink: '',
+            languages: '',
+            picture: 
+        },
+        {
+            title: '',
+            appLink: '',
+            repoLink: '',
+            languages: '',
+            picture: 
+        },
+        {
+            title: '',
+            appLink: '',
+            repoLink: '',
+            languages: '',
+            picture: 
+        },
+        {
+            title: '',
+            appLink: '',
+            repoLink: '',
+            languages: '',
+            picture: 
+        },
+        {
+            title: '',
+            appLink: '',
+            repoLink: '',
+            languages: '',
+            picture: 
+        }
+    ];
