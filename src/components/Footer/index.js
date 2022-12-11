8efe74b755a2ca46bc23d04dc4d67b8da4302823
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-10 21:00:58 -0500

    added the final layout to use for the footer index js file

---
+++ src/components/Footer/index.js
@@ -0,0 +1,31 @@
+import { SocialIcon } from "react-social-icons";
+
+function Footer() {
+    return (
+
+        <footer>
+
+            <div className="socials">
+
+                <div>
+                    <SocialIcon
+                        url=""
+                        target="_blank"
+                    />
+                </div>
+
+                <div>
+                    <SocialIcon
+                        url=""
+                        target="_blank"
+                    />
+                </div>
+
+            </div>
+
+        </footer>
+        
+    );
+}
+
+export default Footer;
