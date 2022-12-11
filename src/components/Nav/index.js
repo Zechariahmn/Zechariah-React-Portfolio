9b3113505a8b7406717205eb648140c10d80335e
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-10 20:59:23 -0500

    completed nav index js file

---
+++ src/components/Nav/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function Nav(props) {
+    const tabs = ['Home', 'About', 'Portfolio', 'Contact']
+
+    return (
+
+        <header id="flex-row">
+
+            {/* <h2>
+                <a href=""></a>
+            </h2> */}
+
+            <nav className="flex-row">
+
+            <ul className="nav-row">
+
+                {tabs.map((tab) => (
+                    <li className="mx-2" key={tab}>
+
+                        <a
+                            href={'#' + tab.toLowerCase()}
+                            onClick={() => props.handlePageChange(tab)}
+                            className={
+                                props.currentPage === tab
+                                    ? "navActive"
+                                    : "mx-2"
+                            }
+
+                        >
+                            {tab}
+
+                        </a>
+
+                    </li>
+
+                ))}
+
+            </ul>
+
+        </nav>
+
+    </header>
+    
+    );
+}
+
+export default Nav;
