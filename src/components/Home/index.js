9e95e593a40905f4bef62d084a0c9b4b2430d235
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-10 20:40:39 -0500

    added the home index js file layout to be used

---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Home() {
+    return (
+
+        <section>
+
+            <div className="hero"></div>
+
+            <div className="social-line">
+
+                <h1 className="titles"></h1>
+
+                <br/>
+
+                <h2 className="titles"> <span></span>.  
+                </h2>
+
+            </div>
+
+        </section >
+    );
+}
+export default Home;
