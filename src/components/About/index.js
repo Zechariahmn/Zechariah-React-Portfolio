1dce0b15f9d72e43ac0acd557fa225092779a48f
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-11 11:26:39 -0500

    changed text color in about file

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -18,13 +18,7 @@ function About() {
 
                     <h2>About Me</h2>
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-                        condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-                        mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-                        Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-                        justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-                        dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
-                        Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+                    <p> I am a web developer in the making that is constantly looking and trying to learn new things in order to make me a better developer and to make my ideas a reality. I have always wanted to try coding but never got the chance when i was younger, so now im diving head first into it and trying to absorb as much as i can.
                     </p>
 
                     <div className="row">
