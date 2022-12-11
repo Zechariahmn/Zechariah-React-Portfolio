27669b45a1a2e60b88476efa653c4d52c2187732
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-10 20:29:44 -0500

    added a function for the contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,1 +1,9 @@
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
+function Contact() {
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
