e7299cbff3f42067d794c332fed5e85da978d8a5
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-10 20:32:13 -0500

    added layout for contact index js part 2

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,3 +7,36 @@ function Contact() {
         email: "",
         message: "",
     });
+
+    const [errorMessage, setErrorMessage] = useState("");
+    const { name, email, message } = formState;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log("Submit Form", formState);
+        }
+    };
+
+    const handleChange = (e) => {
+        if (e.target.name === "email") {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage("Your email is invalid.");
+            } else {
+                setErrorMessage("");
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`Please enter your ${e.target.name}.`);
+            } else {
+                setErrorMessage("");
+            }
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+            console.log("Handle Form", formState);
+        }
+    };
+
+    
