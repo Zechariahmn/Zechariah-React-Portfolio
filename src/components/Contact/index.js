8e1f6c2fa5a3cffbbcef7f7c1309242a1154917d
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-10 20:34:34 -0500

    added contact index js layout part 3

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -39,4 +39,73 @@ function Contact() {
         }
     };
 
-    
+    return (
+        <section className="my-5">
+            <h1 className="titles">
+                Contact Me
+            </h1>
+            <hr></hr>
+            <div className="boxes">
+                <form id="contact-form" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="John Smith"
+                            defaultValue={name}
+                            onBlur={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email address:</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="john.smith@gmail.com"
+                            defaultValue={email}
+                            onBlur={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message:</label>
+                        <textarea
+                            name="message"
+                            rows="5"
+                            defaultValue={message}
+                            onBlur={handleChange}
+                        />
+                    </div>
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
+                    <button data-testid="button" type="submit">
+                        Submit
+                    </button>
+                </form>
+            </div>
+
+            <div className="boxes">
+                <h3 className="titles">Let's talk</h3>
+                <ul>
+                    <li>
+                    <a className="contacts" href="tel:\"></a>
+                    </li>
+                    <li>
+                    <a className="contacts" href="mailto:"></a>
+                    </li>
+                    <li>
+                    <a className="contacts" href="" target="_blank" rel="noreferrer">GitHub</a> 
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
+}
+
+
+
+
+export default Contact;
